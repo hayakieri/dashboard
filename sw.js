20084da936c8d7569fbf9ca057a6e23785b06c05
hayakieri <wexiye0806@gmail.com>
2026-03-06 13:30:51 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    v8 — 强制清除旧缓存
    ============================================================ */
 
-const CACHE = 'dashboard-v11';
+const CACHE = 'dashboard-v12';
 const STATIC = [
   './',
   './index.html',
